b19be286f410f23a53f772e5cc884055c93f0e71
DataSurfer <datasurfer@gmail.com>
2012-09-27 17:53:37 -0400

    allow specifying timestamp value

--- lib/km_resque.rb
+++ lib/km_resque.rb
@@ -12,13 +12,13 @@ class KmResque
   def self.configuration
     @configuration ||= Configuration.new
   end
-  def self.alias(identifier1, identifier2)
-    ::Resque.enqueue(AliasJob, identifier1, identifier2, Time.now.to_i)
+  def self.alias(identifier1, identifier2, timestamp=Time.now.to_i)
+    ::Resque.enqueue(AliasJob, identifier1, identifier2, timestamp)
   end
-  def self.set(identifier, properties)
-    ::Resque.enqueue(SetJob, identifier, properties, Time.now.to_i)
+  def self.set(identifier, properties, timestamp=Time.now.to_i)
+    ::Resque.enqueue(SetJob, identifier, properties, timestamp)
   end
-  def self.record(identifier, eventName, properties)
-    ::Resque.enqueue(RecordJob, identifier, eventName, properties, Time.now.to_i)
+  def self.record(identifier, eventName, properties, timestamp=Time.now.to_i)
+    ::Resque.enqueue(RecordJob, identifier, eventName, properties, timestamp)
   end
 end
--- spec/lib/km_resque/resque_spec.rb
+++ spec/lib/km_resque/resque_spec.rb
@@ -6,6 +6,7 @@ describe "KmResque" do
   before(:each) do
     ResqueSpec.reset!
     Time.stub!(:now => Time.now)
+    @prior_timestamp = Time.now.to_i-(60 * 60 * 24)
     @timestamp = Time.now.to_i
   end
   describe "configuring" do
@@ -17,7 +18,7 @@ describe "KmResque" do
     end
   end
   describe "alias" do
-    it "should queue an AliasJob" do
+    it "should queue an AliasJob with current time" do
       KmResque.alias("identifier1", "identifier2")
       KmResque::AliasJob.should have_queued("identifier1",
                                               "identifier2",
@@ -26,7 +27,7 @@ describe "KmResque" do
     end
   end
   describe "set" do
-    it "should queue an SetJob" do
+    it "should queue a SetJob with current time" do
       KmResque.set("identifier", {:some_prop => 'some_val'})
       KmResque::SetJob.should have_queued("identifier",
                                             {:some_prop => 'some_val'},
@@ -35,12 +36,40 @@ describe "KmResque" do
     end
   end
   describe "record" do
-    it "should queue an RecordJob" do
+    it "should queue a RecordJob with current time" do
       KmResque.record("identifier", "myEventName", {:some_prop => 'some_val'})
       KmResque::RecordJob.should have_queued("identifier",
                                                "myEventName",
                                                {:some_prop => 'some_val'},
                                                @timestamp
+                                               ).in(:km)
+    end
+  end
+  describe "alias" do
+    it "should queue an AliasJob with arbitrary timestamp" do
+      KmResque.alias("identifier1", "identifier2", @prior_timestamp)
+      KmResque::AliasJob.should have_queued("identifier1",
+                                              "identifier2",
+                                              @prior_timestamp
+                                              ).in(:km)
+    end
+  end
+  describe "set" do
+    it "should queue a SetJob with arbitrary timestamp" do
+      KmResque.set("identifier", {:some_prop => 'some_val'}, @prior_timestamp)
+      KmResque::SetJob.should have_queued("identifier",
+                                            {:some_prop => 'some_val'},
+                                            @prior_timestamp
+                                            ).in(:km)
+    end
+  end
+  describe "record" do
+    it "should queue a RecordJob with arbitrary timestamp" do
+      KmResque.record("identifier", "myEventName", {:some_prop => 'some_val'}, @prior_timestamp)
+      KmResque::RecordJob.should have_queued("identifier",
+                                               "myEventName",
+                                               {:some_prop => 'some_val'},
+                                               @prior_timestamp
                                                ).in(:km)
     end
   end
